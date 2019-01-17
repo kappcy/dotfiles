f0a06f5be1010f1092bace268c8eb78463c1a95d
kappcy <corey7144@gmail.com>
2019-01-16 20:33:41 -0600

    adding openbox stuffs

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,27 @@
+#----------Desktop----------
+
+sleep 1 && nitrogen --restore & #background
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & #polkit
+/usr/lib/xfce4/notifyd/xfce4-notifyd & #notifications
+ckb-next -b & #Corsair_Mouse
+numlockx on #numlock
+compton & #compositor
+quicktile --daemonize & #tiling
+~/.config/polybar/launchall.sh & #panels
+~/scripts/openbox/screensaver.sh & #my_screen-blanking_script
+
+#----------SysTray----------
+
+redshift-gtk & #nightmode
+nm-applet & #network
+clipit & #clipboard_manager
+
+
+#-----------apps------------
+
+discord &
+steam -silent &
+pcmanfm &
+termite &
+vivaldi-stable &
+sleep 3 && ~/scripts/autotile & #my_handy_startup_tiled_script
