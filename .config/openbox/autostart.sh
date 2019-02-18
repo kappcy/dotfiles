7261577a65e5ee2372f21393fc58d789b175f2cb
kappcy <corey7144@gmail.com>
2019-02-17 22:12:25 -0600

    added new script

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,10 +1,12 @@
 #----------Desktop----------
 
 sleep 1 && nitrogen --restore & #background
+sleep 3 && ~/scripts/xwinwrap-wallpaper.sh & #animated_wallpaper
+
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & #polkit
 /usr/lib/xfce4/notifyd/xfce4-notifyd & #notifications
 ckb-next -b & #Corsair_Mouse
-numlockx on #numlock
+numlockx on & #numlock
 compton & #compositor
 quicktile --daemonize & #tiling
 ~/.config/polybar/launchall.sh & #panels
@@ -15,7 +17,7 @@ quicktile --daemonize & #tiling
 redshift-gtk & #nightmode
 nm-applet & #network
 clipit & #clipboard_manager
-
+shutter --min_at_startup & #screencapture_utility
 
 #-----------apps------------
 
