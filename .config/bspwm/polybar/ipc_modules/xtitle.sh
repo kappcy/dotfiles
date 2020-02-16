6b8fe973aaaf1b25a845da4452ffdc14bbe4884e
kappcy <corey7144@gmail.com>
2020-02-15 19:09:58 -0600

    fixed issues with title having special characters

--- .config/bspwm/polybar/ipc_modules/xtitle.sh
+++ .config/bspwm/polybar/ipc_modules/xtitle.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 focus_monitor="bspc monitor pointed -f"
 
-get_title() {
+get_info() {
 	xid=$(bspc query -N -n .active -m $1 2>/dev/null)
 	title="$(xtitle $xid)"
 	[[ -z $xid ]] && unset title
-	read -t "0.02" <> <(:)
+	read -t "0.01" <> <(:)
 }
 
 update() {
-	focused_node="$(bspc query -N -n)"
-	get_title $1
-	active_list+=($1='"'$xid'"')
-	[[ ${last_active_list[@]} != *$1='"'$xid'"'* ]] && \
-	[[ ${active_list[@]} = *$1='"'$xid'"'* ]] && \
+	get_info $1
+	title=${title//[^[:alnum:]]/SC}
+	title_list+=($1=$title)
+	[[ ${last_title_list[@]} != *$1=$title* ]] && \
+	[[ ${title_list[@]} = *$1=$title* ]] && \
 	polybar-msg hook xtitle_$2 1
 }
 
 case "$1" in
 	start)
 		xtitle -s | while read; do
-			unset active_list
+			unset title_list
 			update DVI-D-0 right
 			update HDMI-0 left
 			update DP-0 tv
-			last_active_list=(${active_list[@]})
+			last_title_list=(${title_list[@]})
 		done
 	;;
 	set_title)
-		get_title $2
+		get_info $2
 		[[ -z $xid ]] && exit
 		echo -ne "%{A1:$focus_monitor:}"
-		echo -ne "%{A2:$focus_monitor; bspc node $focused_node -c:}"
+		echo -ne "%{A2:$focus_monitor; bspc node -c:}"
 		echo -ne "%{T3}%{F#b8bb26}  %{F-}%{T-}"
 		echo -ne "$title"
 		echo -ne "%{A}%{A}"
